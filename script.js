c34616d373bfc24a5d69cabbf538f3768fbfe182
Watcharakorn <144198724+ironmanwin1@users.noreply.github.com>
2025-03-29 16:39:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function updateClock() {
+  const now = new Date();
+  const clock = document.getElementById("clock");
+  clock.textContent = now.toLocaleTimeString();
+}
+
+setInterval(updateClock, 1000);
+updateClock();
